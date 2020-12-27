ba7c86eddbb5bc1d1d06d0e6cb14e5b4c965db10
tuda-suda <marat.gumerov99@gmail.com>
2020-12-27 13:12:53 +0500

    Auth templates

--- users/urls.py
+++ users/urls.py
@@ -8,40 +8,40 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
         'login/',
-        auth_views.LoginView.as_view(template_name='authForm.html'),
+        auth_views.LoginView.as_view(template_name='auth/authForm.html'),
         name='login'
     ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path(
         'password-change/',
         auth_views.PasswordChangeView.as_view(
-            template_name='changePassword.html'
+            template_name='auth/changePassword.html'
         ),
         name='password_change'
     ),
     path(
         'password-change-done/',
         auth_views.PasswordChangeDoneView.as_view(
-            template_name='changePasswordDone.html'
+            template_name='auth/changePasswordDone.html'
         ),
         name='password_change_done'
     ),
     path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='resetPassword.html'
+        template_name='auth/resetPassword.html'
         ),
         name='password_reset'
     ),
     path(
         'password-reset-done/',
         auth_views.PasswordResetDoneView.as_view(
-            template_name='resetPasswordDone.html'
+            template_name='auth/resetPasswordDone.html'
         ),
         name='password_reset_done'
     ),
     path(
         'password-reset-confirm/',
         auth_views.PasswordResetConfirmView.as_view(
-            template_name='resetPasswordConfirm.html'
+            template_name='auth/resetPasswordConfirm.html'
         ),
         name='password_reset_confirm'
     ),
--- users/views.py
+++ users/views.py
@@ -7,4 +7,4 @@ from .forms import CreationForm
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('login')
-    template_name = 'reg.html'
+    template_name = 'auth/signUp.html'
