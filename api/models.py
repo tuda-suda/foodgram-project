76e3d2b50952bed385b0e8b438fa4df9d1f7e284
tuda-suda <marat.gumerov99@gmail.com>
2021-01-03 13:06:14 +0500

    Add docs

--- api/models.py
+++ api/models.py
@@ -8,47 +8,69 @@ User = get_user_model()
 
 
 class Favorite(models.Model):
+    """
+    Stores a favorite relation between `auth.User` and `recipes.Recipe`.
+    """
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='favorites'
+        related_name='favorites',
+        verbose_name='Пользователь',
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='favored_by'
+        related_name='favored_by',
+        verbose_name='Рецепт в избранном',
     )
 
     class Meta:
         unique_together = ('user', 'recipe')
+        verbose_name = 'Избранный рецепт'
+        verbose_name_plural = 'Избранные рецепты'
 
 
 class Subscription(models.Model):
+    """
+    Stores a subscription relation between two `auth.User`.
+    User is subscribed to author.
+    """
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='follower'
+        related_name='follower',
+        verbose_name='Подписался на',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='following'
+        related_name='following',
+        verbose_name='Подписчик',
     )
 
     class Meta:
         unique_together = ('user', 'author')
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
 
 
 class Purchase(models.Model):
+    """
+    Stores a purchase relation between `auth.User` and `recipes.Recipe`.
+    """
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='purchases'
+        related_name='purchases',
+        verbose_name='Пользователь',
     )
     recipe = models.ForeignKey(
         Recipe,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт в покупках',
     )
 
     class Meta:
         unique_together = ('user', 'recipe')
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
