0263ea01aa0631aff382fc2ddf8cb3b9c2df3a09
tuda-suda <marat.gumerov99@gmail.com>
2021-01-10 11:39:06 +0500

    Fix shoplist counter and activity

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'recipes.context_processors.shop_list_size'
             ],
         },
     },
---
+++ recipes/context_processors.py
@@ -0,0 +1,9 @@
+def shop_list_size(request):
+    if request.user.is_authenticated:
+        count = request.user.purchases.all().count()
+    else:
+        count = 0
+        
+    return {
+        "shop_list_size": count
+    }
