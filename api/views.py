0982162c8d9abfb59e5c7dc8ae3698b04a59164b
tuda-suda <marat.gumerov99@gmail.com>
2021-01-10 12:33:15 +0500

    Fix Delete method errors

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -17,8 +18,22 @@ class CreateDestroyViewSet(mixins.CreateModelMixin,
 
     `destroy` action is overriden to return a json with a `success` flag.
     """
+    def get_object(self, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        filter_kwargs = {
+            self.lookup_field: self.kwargs[lookup_url_kwarg],
+            **kwargs,
+        }
+        
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
     def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
+        instance = self.get_object(user=self.request.user)
         success = instance.delete()
         return Response({'success': bool(success)}, status=status.HTTP_200_OK)
 
